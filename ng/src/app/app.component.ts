31a05e38d27f3ed95d3d2056c048423ac95bda5a
Nabil <nabil.dridi@gmail.com>
2020-09-20 20:29:11 +0100

    Corrections

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LazyLoadEvent } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   columnsDefs = [
